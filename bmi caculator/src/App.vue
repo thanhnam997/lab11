ad4afee30af35e568cfce57b7c24e75d47496708
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-04-03 19:57:48 -0500

    update

---
+++ bmi caculator/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import BodyMassindexform from './components/BodyMassindexform.vue';
+</script>
+
+<template>
+<h1>Body Mass Index Caculator</h1>
+ hello from app.vue!
+
+ <BodyMassindexform/>
+
+
+</template>
+
+<style scoped>
+
+</style>./components/BodyMassindexform.vue
---
+++ bmi caculator/src/components/BodyMassindexform.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+
+
+const count = ref(0)
+</script>
+
+<template>
+<h2>from to enter your data</h2>
+hello from BodyMassindexform
+
+<label>Enter height in meter</label>
+<input>
+
+</template>
+
+<style scoped>
+
+</style>
