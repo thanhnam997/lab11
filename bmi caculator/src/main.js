9fc867f1335351926ae1d468623fb3699a16451d
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-04-03 20:08:21 -0500

    Update main.js

--- bmi caculator/src/main.js
+++ bmi caculator/src/main.js
@@ -4,3 +4,13 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+//create app...
+const app = createApp(App)
+
+//....configure App...
+//... to use pinia store
+const pinia = createPinia()
+app.use(pinia)
+
+//...mount(load) configured app in browser
+app.mount
