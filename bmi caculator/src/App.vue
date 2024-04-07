56715cedb940a4af1614a36ca8cfbc7198b5d149
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-04-07 09:17:36 -0500

    Update App.vue

--- bmi caculator/src/App.vue
+++ bmi caculator/src/App.vue
@@ -1,16 +1,1 @@
-<script setup>
-import BodyMassindexform from './components/BodyMassindexform.vue';
-</script>
-
-<template>
-<h1>Body Mass Index Caculator</h1>
- hello from app.vue!
-
- <BodyMassindexform/>
-
-
-</template>
-
-<style scoped>
-
-</style>./components/BodyMassindexform.vue
+cd
